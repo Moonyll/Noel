7ae430b8d7148d5456bbf0680a983850bfa3d25e
Moonyll <s.pouwel@laposte.net>
2019-01-02 09:39:56 +0100

    2019 Update

--- Program.cs
+++ Program.cs
@@ -11,54 +11,60 @@ namespace Noel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\x02");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello, choose your christmas gift !" +"\n");
+            Console.SetCursorPosition(10, 0); // Décalage du texte de 10 à gauche...
+            Console.ForegroundColor = ConsoleColor.Green; // Mise en couleur des textes...
+            Console.WriteLine("\x02" + " "+ "Hello ! It's Christmas time ! So pick your christmas gift !" +"\n"); // \x.. + N° pour créer un symbole
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("1. A Snowman..." + "\n");
+            Console.WriteLine("1. A Snowman... " + "\x01"+ "\n");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("2. An Elf..." + "\n");
+            Console.WriteLine("2. An Elf... " + "\x04" + "\n");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("3. Mr. Santa Claus..." + "\n");
+            Console.WriteLine("3. Mr. Santa Claus... " + "\x03" + "\n");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("4. The Reindeer..." + "\n");
+            Console.WriteLine("4. The Reindeer... " + "\x05" + "\n");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("5. Just A Tree..." + "\n");
+            Console.WriteLine("5. Just A Tree... " + "\x06" + "\n");
             Console.ForegroundColor = ConsoleColor.Gray; string choice = Console.ReadLine();
-            if (choice == "1")
+            if (choice == "1") // Création des boucles if
             {
+                Console.SetCursorPosition(10, 20); // Décalage du texte de 10 à gauche & 20 du haut...
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\x01" + " " + "\x01" + " Merry Christmas from Snowman ! "+"\x01" + " " + "\x01");
+                Console.WriteLine("\x01" + " " + "\x01" + " Brrrr ! Snowman wish you a Merry Christmas ! " + "\x01" + " " + "\x01");
                 Console.ReadKey();
-                
             }
             else if (choice == "2")
             {
+                Console.SetCursorPosition(10, 20);
                 Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("\x04" + " " + "\x04" +" Merry Christmas From The Elf ! " +"\x04"+" "+ "\x04");
+                Console.WriteLine("\x04" + " " + "\x04" +" Shamrock ! Elf wish you a Merry Christmas ! " +"\x04"+" "+ "\x04");
                 Console.ReadKey();
             }
             else if (choice == "3")
             {
+                Console.SetCursorPosition(10, 20);
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\x03" + " " + "\x03" + " Merry Christmas Santa Claus ! "+ "\x03" + " " + "\x03");
+                Console.WriteLine("\x03" + " " + "\x03" + " Oh Oh Oh ! Santa Claus wish you a Merry Christmas ! " + "\x03" + " " + "\x03");
                 Console.ReadKey();
             }
             else if (choice == "4")
             {
+                Console.SetCursorPosition(10, 20);
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("\x05" + " " + "\x05" + " Merry Christmas from Reindeer  ! " + "\x05" + " " + "\x05");
+                Console.WriteLine("\x05" + " " + "\x05" + " Hi Ho ! Reindeer wish you a Merry Christmas ! " + "\x05" + " " + "\x05");
                 Console.ReadKey();
             }
             else if (choice == "5")
             {
+                Console.SetCursorPosition(10, 20);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("\x06" + " " + "\x06" + " Merry Christmas from Tree ! "+"\x06" + " " + "\x06");
+                Console.WriteLine("\x06" + " " + "\x06" + " I'm a Tree, nevertheless i wish you Merry Christmas ! " + "\x06" + " " + "\x06");
                 Console.ReadKey();
             }
             else
             {
-                Console.WriteLine("Oups, wrong choice !");
+                Console.ForegroundColor = ConsoleColor.Blue; // Message d'erreur si mauvais choix...
+                Console.SetCursorPosition(10, 20);
+                Console.WriteLine("\x02" + " " +"Ding Dong ! Oups, you've choose a wrong choice !");
             }
         }
 
